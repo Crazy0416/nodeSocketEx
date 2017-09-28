5f38db8dcd5faeb2b120331383b9079c0ba7bbfc
Min-ho Kim <minmin0416@naver.com>
2017-09-29 02:35:16 +0900

    Add tmp router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
+router.get('/tmp', function(req, res){
+  res.send("hello world");
+})
+
 module.exports = router;
